7d109ecee996bf0f70935dda5c1a206bc7098fcf
matdadi <49846800+matdadi@users.noreply.github.com>
2022-08-19 10:22:59 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -24,18 +24,18 @@ def load_menu():
 
     with menu_1, _lock:
         st.markdown('''<div style="text-align: center; margin-bottom: 100px;"><a target="_self"
-            href=https://share.streamlit.io/matdadi/hivaidsjabar/main/hivjabar.py?p='''+load_master('menu')[0]+
+            href=https://matdadi-hivaidsjabar-hivjabar-6g57cz.streamlitapp.com/?p='''+load_master('menu')[0]+
             ''' style='''+selected_style[0]+'''>'''+load_master('menu')[0]+'''</a></div>
         ''', unsafe_allow_html=True)
 
     with menu_2, _lock:
         st.markdown('''<div style="text-align: center; margin-bottom: 100px;"><a target="_self"
-            href=https://share.streamlit.io/matdadi/hivaidsjabar/main/hivjabar.py?p='''+load_master('menu')[1]+
+            href=https://matdadi-hivaidsjabar-hivjabar-6g57cz.streamlitapp.com/?p='''+load_master('menu')[1]+
             ''' style='''+selected_style[1]+'''>'''+load_master('menu')[1]+'''</a></div>
         ''', unsafe_allow_html=True)
 
     with menu_3, _lock:
         st.markdown('''<div style="text-align: center; margin-bottom: 100px;"><a target="_self"
-            href=https://share.streamlit.io/matdadi/hivaidsjabar/main/hivjabar.py?p='''+load_master('menu')[2]+
+            href=https://matdadi-hivaidsjabar-hivjabar-6g57cz.streamlitapp.com/?p='''+load_master('menu')[2]+
             ''' style='''+selected_style[2]+'''>'''+load_master('menu')[2]+'''</a></div>
         ''', unsafe_allow_html=True)
